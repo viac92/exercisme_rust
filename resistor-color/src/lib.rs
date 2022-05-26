3fc40d9f094f580e9ec6871834766c649cfd2780
viac92 <64146594+viac92@users.noreply.github.com>
2022-05-26 12:17:58 +0200

    impl color_to_value

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -13,7 +13,18 @@ pub enum ResistorColor {
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+    match _color {
+        ResistorColor::Black => 0,
+        ResistorColor::Brown => 1,
+        ResistorColor::Red => 2,
+        ResistorColor::Orange => 3,
+        ResistorColor::Yellow => 4,
+        ResistorColor::Green => 5,       
+        ResistorColor::Blue => 6,
+        ResistorColor::Violet => 7,
+        ResistorColor::Grey => 8,
+        ResistorColor::White => 9,
+    }
 }
 
 pub fn value_to_color_string(value: usize) -> String {
