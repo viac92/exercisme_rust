f043510360b919f92401428e84f592161207ca12
viac92 <64146594+viac92@users.noreply.github.com>
2022-05-26 16:04:57 +0200

    resolve exercise

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -1,39 +1,49 @@
-#[derive(Debug, PartialEq)]
+use int_enum::IntEnum;
+use enum_iterator::IntoEnumIterator;
+
+
+#[repr(usize)]
+#[derive(Debug, PartialEq, Copy, Clone, IntEnum, IntoEnumIterator)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    match _color {
-        ResistorColor::Black => 0,
-        ResistorColor::Brown => 1,
-        ResistorColor::Red => 2,
-        ResistorColor::Orange => 3,
-        ResistorColor::Yellow => 4,
-        ResistorColor::Green => 5,       
-        ResistorColor::Blue => 6,
-        ResistorColor::Violet => 7,
-        ResistorColor::Grey => 8,
-        ResistorColor::White => 9,
-    }
+    int_enum::IntEnum::int_value(_color)
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match value {
+        0 => "Black".to_string(),
+        1 => "Brown".to_string(),
+        2 => "Red".to_string(),
+        3 => "Orange".to_string(),
+        4 => "Yellow".to_string(),
+        5 => "Green".to_string(),
+        6 => "Blue".to_string(),
+        7 => "Violet".to_string(),
+        8 => "Grey".to_string(),
+        9 => "White".to_string(),
+        _ => "value out of range".to_string(),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    let iter = ResistorColor::into_enum_iter();
+    let mut result: Vec<ResistorColor> = [].to_vec();
+
+    for item in iter {
+        result.push(item);
+    }
+
+    result
 }
